4e7fe01c5411dc907fca750b46159fb7444d7277
Erick Wendel <erick.workspace@gmail.com>
2024-07-20 17:20:56 -0300

    add consume on demand

--- sheets-to-json-stream.js
+++ sheets-to-json-stream.js
@@ -1,41 +1,90 @@
 const google = require('@googleapis/sheets');
 const { JWT } = require('google-auth-library');
+const { Readable } = require('stream')
 const SCOPES = [
     'https://www.googleapis.com/auth/spreadsheets.readonly',
-]
+];
 
+/**
+ * Class to fetch and process Google Sheets data.
+ */
 class SheetsToJSON {
-    #sheets = {}
-    metadata = new Map()
-    #google
-    #JWT
+    /**
+   * @type {import('@googleapis/sheets').sheets_v4.Sheets}
+   * @private
+   */
+    #sheets;
 
+    /**
+     * @type {Map}
+     * @public
+     */
+    metadata = new Map();
+
+    /**
+     * @type {typeof import('@googleapis/sheets')}
+     * @private
+     */
+    #google;
+
+    /**
+     * @type {import('google-auth-library').JWT}
+     * @private
+     */
+    #JWT;
+
+    /**
+     * Creates an instance of SheetsToJSON.
+     * @param {Object} param - The parameter object.
+     * @param {Object} param.google - The Google APIs client.
+     * @param {Object} param.JWT - The Google Auth JWT client.
+     */
     constructor({ google, JWT }) {
-        this.#google = google
-        this.#JWT = JWT
+        this.#google = google;
+        this.#JWT = JWT;
     }
 
-    async getSheetDataByRange({ spreadsheetId, range }) {
-        const valuesResponse = await this.#sheets.spreadsheets.values.get({
-            spreadsheetId,
-            range,
-        });
+    /**
+    * Fetches sheet data by range.
+    * @param {Object} param - The parameter object.
+    * @param {string} param.spreadsheetId - The ID of the spreadsheet.
+    * @param {string} param.range - The A1 notation of the range to retrieve values from.
+    * @returns {Promise<Object|import('stream').Readable>} - A promise that resolves to the sheet data as a readable stream.
+    */
+    async getSheetDataByRange({ spreadsheetId, range }, asStream = false) {
+        const options = asStream ? { responseType: 'stream' } : {}
+        const response = await this.#sheets.spreadsheets.values.get(
+            {
+                spreadsheetId,
+                range,
+            },
+            options
+        );
+        if (asStream)
+            return Readable.from(response.data, { encoding: 'utf-8' });
 
-        return valuesResponse
+        return response.data
     }
 
+
+    /**
+     * Fetches metadata of the sheets in the spreadsheet.
+     * @param {Object} param - The parameter object.
+     * @param {string} param.spreadsheetId - The ID of the spreadsheet.
+     * @returns {Promise<Object>} - The metadata of the sheets.
+     */
     async getSheetsMetadata({ spreadsheetId }) {
-        const response = await this.#sheets.spreadsheets.get({
+        const { data: response } = await this.#sheets.spreadsheets.get({
             spreadsheetId,
         });
 
-        const sheetsRequest = response.data.sheets.map(async sheet => {
+        const sheetsRequest = response.sheets.map(async sheet => {
             const sheetTitle = sheet.properties.title;
 
             // Fetch the first row to use as headers
             const range = `${sheetTitle}!A1:1`;
             const valuesResponse = await this.getSheetDataByRange({ spreadsheetId, range })
-            const headers = valuesResponse.data.values ? valuesResponse.data.values[0] : [];
+            const headers = valuesResponse.values ? valuesResponse.values[0] : [];
 
             // Calculate the cell range in A1 notation
             const rowCount = sheet.properties.gridProperties.rowCount;
@@ -52,17 +101,22 @@ class SheetsToJSON {
                 headers: headers,
                 cellsRange: cellsRange,
             };
-        })
+        });
         const sheetData = await Promise.all(sheetsRequest);
 
         const metadata = {
-            title: response.data.properties.title,
+            title: response.properties.title,
             sheets: sheetData,
         };
 
         return metadata;
     }
 
+    /**
+     * Converts a column number to its corresponding letter(s).
+     * @param {number} columnNumber - The column number.
+     * @returns {string} - The corresponding column letter(s).
+     */
     getColumnLetter(columnNumber) {
         let letter = '';
         while (columnNumber > 0) {
@@ -73,6 +127,14 @@ class SheetsToJSON {
         return letter;
     }
 
+    /**
+     * Fetches and sets the sheets metadata.
+     * @param {Object} googleSheetsConfig - The Google Sheets API configuration.
+     * @param {string} googleSheetsConfig.client_email - The client email.
+     * @param {string} googleSheetsConfig.private_key - The private key.
+     * @param {string} spreadsheetId - The ID of the spreadsheet.
+     * @returns {Promise<Object>} - The sheets metadata.
+     */
     async fetchSheetsMetadata(googleSheetsConfig, spreadsheetId) {
         const auth = new this.#JWT({
             email: googleSheetsConfig.client_email,
@@ -85,13 +147,15 @@ class SheetsToJSON {
         const metadata = await this.getSheetsMetadata({ spreadsheetId });
         return metadata;
     }
-
 }
 
 
-module.exports = function (RED) {
+
+module.exports = function main(RED) {
 
     const sheetsToJSON = new SheetsToJSON({ google, JWT })
+
+
     async function onSheetOptionsRequest(req, res) {
         try {
 
@@ -128,31 +192,44 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, ctx);
         const node = this;
         const context = {
-            sheetId: ctx.sheetId,
-            range: ctx.range,
-            columns: ctx.columns
+            spreadsheetId: ctx.sheetId,
+            range: `${ctx.sheetList}!${ctx.range}`,
+            columns: ctx.columns,
+            config: ctx.config,
         }
-
         // const columns = ctx.columns
-        const creds = RED.nodes.getNode(ctx.credentials);
+        const creds = JSON.parse(RED.nodes.getNode(context.config).credentials.config);
+        node.on('input', async function (msg) {
+            try {
+                const metadata = await sheetsToJSON.fetchSheetsMetadata(creds, context.spreadsheetId)
+                const stream = await sheetsToJSON.getSheetDataByRange(context)
+                let headers = []
 
-        node.on('input', function (msg) {
-            // const isRange = typeof columns[0] === 'string'
-            // const meta = sheetsMetadata.get(sheetId)
-            // if (!meta) return node.error('couldnt find the sheet')
+                for (const line of stream.values) {
+                    console.log('lengh', line.length)
+                    if (!headers.length) {
+                        headers = line
+                        continue
+                    }
 
-            // node.warn(meta)
+                    const lineItem = {}
+                    for (const indexLine in line) {
+                        const headerName = headers[indexLine]
+                        // if (!context.columns.includes(headerName) && headerName) continue
 
-            // if (isRange) {
-            //     node.warn('handle as range')
-            // }
-            // else {
-            //     // all the file
-            //     // parse each line
-            //     //  parse 1000 items per time
+                        lineItem[headerName] = line[indexLine]
+                    }
 
-            //     node.warn('handle columns')
-            // }
+                    node.send({ payload: lineItem });
+                }
+                // console.log('item', item)
+                // node.send(item);
+
+            } catch (error) {
+                console.log('error', error)
+                node.send({ payload: 'error' });
+
+            }
 
             node.send(msg);
         });
